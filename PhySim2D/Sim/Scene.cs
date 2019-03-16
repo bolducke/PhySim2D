c4c74187450ebeb7238cfde09d8a1598787ce11a
LodrikMtl <bobibibidit@hotmail.com>
2019-03-16 14:44:48 -0400

    Change testing scene

--- PhySim2D/Sim/Scene.cs
+++ PhySim2D/Sim/Scene.cs
@@ -28,17 +28,17 @@ namespace PhySim2D.Sim
         internal event EventHandler<ContactInCollisionEventArgs> ContactsInCollision;
 
         Rigidbody rect;
+        Rigidbody c;
 
-        
         public Scene()
         {
-            KTransform t1 = new KTransform(new KVector2(0f,5f), 1f, new KVector2(2));
-            KTransform t2 = new KTransform(new KVector2(0, -5.5f), 0f, new KVector2(1));
-            KTransform t3 = new KTransform(new KVector2(0f, 2.1f), 0f, new KVector2(1));
+            KTransform t1 = new KTransform(new KVector2(0f, 2f), 1f, new KVector2(3, 1));
+            KTransform t2 = new KTransform(new KVector2(0, 0f), 0f, new KVector2(1, 1));
+            KTransform t3 = new KTransform(new KVector2(0f, 9f), 0.5, new KVector2(2, 1));
             List<Collider> ss = new List<Collider>
             {
-                new Circle(new KVector2(0,0), 0.5f),
-                new Circle(new KVector2(10,4), 0.5f)
+                new Circle(new KVector2(0,0)),
+                new Circle(new KVector2(10,4))
             };
             //Vertices v1 = new Vertices()
             //{
@@ -83,16 +83,17 @@ namespace PhySim2D.Sim
 
             Rigidbody s = BodyFactory.CreatePolygon(t1, new MassData(3, 3), v1);
             Rigidbody s2 = BodyFactory.CreatePolygon(t3, new MassData(1, 3), v1);
-            Rigidbody c = BodyFactory.CreateCircleBody(1f, new MassData(1, 1), t2);
-            //Rigidbody seg = BodyFactory.CreateSegmentBody(new KVector2(-1, 0), new KVector2(1, 0), new MassData(1,1), t2);
-            rect = BodyFactory.CreatePolygon(t2, new MassData(0,0), v);
-            Bodies.Add(s);
+            c = BodyFactory.CreateCircleBody(new MassData(1, 1), t3);
+            Rigidbody seg = BodyFactory.CreateSegmentBody(new KVector2(-1, 0), new KVector2(1, 0), new MassData(1, 1), t2);
+            rect = BodyFactory.CreatePolygon(t2, new MassData(1, 0), v);
+            //Bodies.Add(BodyFactory.CreateCircleBody(new MassData(1, 1), t1));
+            //Bodies.Add(seg);
             Bodies.Add(rect);
             Bodies.Add(c);
             s.AddImpulseAtWPos(new KVector2(3f, 0), s.State.Transform.Position);
 
             s.State.ForceGenerators.Add("G", new GravityHMR(new KVector2(0, -2)));
-            c.State.ForceGenerators.Add("G", new GravityHMR(new KVector2(0,-2)));
+            c.State.ForceGenerators.Add("G", new GravityHMR(new KVector2(0, -2)));
 
             Integrator = new RK4();
         }
@@ -100,10 +101,10 @@ namespace PhySim2D.Sim
         public void DoStep(float step)
         {
             //Forces
-
+            //c.State.Transform.Rotation += 0.001f;
             //Update Position and Velocity
             foreach (Rigidbody body in Bodies)
-                Integrator.Integrate(body,step);
+                Integrator.Integrate(body, step);
 
             //Broadphase
             List<Contact> pairs = broadphaseManager.SpotPotentialCollision(Bodies);
